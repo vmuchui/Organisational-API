1c196327609e102684d91bb1f124edecf4c83386
vmuchui <kaigongimuchui@gmail.com>
2019-07-09 17:05:33 +0300

    add implementation of the save, update, delete and getAll methods.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    void save(User user);
+    User find(int id);
+    List<User> getAll();
+    void update(User user, int id);
+    void delete(User user);
+    void clearAll();
 }
