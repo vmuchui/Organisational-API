19c3ee62c345d9de60a1d4fea3617deadba50449
vmuchui <kaigongimuchui@gmail.com>
2019-07-10 14:44:35 +0300

    remove h2 as one of dependencies

---
+++ src/test/java/models/DataBaseRule.java
@@ -0,0 +1,25 @@
+import models.DB;
+import org.junit.rules.ExternalResource;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+public class DataBaseRule extends ExternalResource{
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/org_api_test", "victor", "Am0skwito");
+
+    }
+    @Override
+    protected void after() {
+        try (Connection con = DB.sql2o.open()){
+            String initializeusers = "DELETE FROM users *;";
+            String initializedepartments = "DELETE FROM departments *;";
+            String initializenews = "DELETE FROM news *;";
+            con.createQuery(initializeusers).executeUpdate();
+            con.createQuery(initializedepartments).executeUpdate();
+            con.createQuery(initializenews).executeUpdate();
+        }
+    }
+
+}
