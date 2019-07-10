3c47fa41eb2896df513f7c1165c2ba966c434de9
vmuchui <kaigongimuchui@gmail.com>
2019-07-10 12:32:18 +0300

    create abstract class news from which other classes inherit.

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,22 @@
 package models;
 
-public class News {
+public abstract class News {
+    private String headlines;
+    private String content;
+
+    public String getHeadlines() {
+        return headlines;
+    }
+
+    public void setHeadlines(String headlines) {
+        this.headlines = headlines;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
