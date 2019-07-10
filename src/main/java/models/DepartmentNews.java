bf5ba1147e07573794b1de7cf8054a251eea7db5
vmuchui <kaigongimuchui@gmail.com>
2019-07-10 12:30:54 +0300

    add class DEpartment news.

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,4 +1,48 @@
 package models;
 
-public class DepartmentNews {
+import java.util.Objects;
+
+public class DepartmentNews extends News {
+    public final String TYPE = "department";
+    private int id;
+    private int departmentId;
+
+    public DepartmentNews(String headlines, String content, int departmentId) {
+        this.setHeadlines(headlines);
+        this.setContent(content);
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DepartmentNews)) return false;
+        DepartmentNews that = (DepartmentNews) o;
+        return getId() == that.getId() &&
+                getDepartmentId() == that.getDepartmentId() &&
+                TYPE.equals(that.TYPE);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(TYPE, getId(), getDepartmentId());
+    }
 }
