46208619db7ec432100aeca0b3ac37497b057f87
vmuchui <kaigongimuchui@gmail.com>
2019-07-10 12:27:20 +0300

    add interface with all methods that need to be implemented.

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,16 @@
 package dao;
 
-public class NewsDao {
+import models.DepartmentNews;
+import models.GeneralNews;
+
+import java.util.List;
+
+public interface NewsDao  {
+    void save(Object o);
+    Object find(int id);
+    List<GeneralNews> getAll();
+    List<DepartmentNews> getDeptNews(int id);
+    void update(Object o, int id);
+    void delete(Object o);
+    void clearAll();
 }
