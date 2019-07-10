3124f75def5e00664005e68f7982a567efe262e3
vmuchui <kaigongimuchui@gmail.com>
2019-07-10 12:37:50 +0300

    add tests for getter and setter methods in general news class.

--- src/test/java/models/GeneralNewsTest.java
+++ src/test/java/models/GeneralNewsTest.java
@@ -1,5 +1,34 @@
+package models;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GeneralNewsTest {
 
+    @Test
+    public void getId() {
+        GeneralNews generalNews = new GeneralNews("OFFICE PARTY", "next week ...");
+        generalNews.setId(2);
+        assertEquals(2, generalNews.getId());
+    }
+
+    @Test
+    public void setId() {
+        GeneralNews generalNews = new GeneralNews("OFFICE PARTY", "next week ...");
+        generalNews.setId(2);
+        assertEquals(2, generalNews.getId());
+    }
+
+    @Test
+    public void getHeadlines() {
+        GeneralNews generalNews = new GeneralNews("OFFICE PARTY", "next week ...");
+        assertEquals("OFFICE PARTY", generalNews.getHeadlines());
+    }
+
+    @Test
+    public void getContent() {
+        GeneralNews generalNews = new GeneralNews("OFFICE PARTY", "next week ...");
+        assertEquals("next week ...", generalNews.getContent());
+    }
 }
